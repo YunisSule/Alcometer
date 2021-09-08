a6bd5c78da15d11d625e6eef98be3856110a4d38
Yunis Sule <yunis.sule@gmail.com>
2021-09-08 14:15:18 +0300

    Wrote Function App() to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const liters = bottles * 0.33;
+    const grams = liters * 8 * 4.5;
+    const burning = weight / 10;
+    let bloodAlcoholLevel = 0;
+    if (gender === 'male') {
+      bloodAlcoholLevel = (grams - burning * time) / (burning * 0.7);
+    } else {
+      bloodAlcoholLevel = (grams - burning * time) / (weight * 0.6);
+    }
+
+    if (bloodAlcoholLevel < 0) {
+      setResult(0);
+    } else {
+      setResult(bloodAlcoholLevel);
+    }
+  }
   return (
     <div>
       <h1>Calculating alcohol blood level</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Weight</label>
+            <input name="weight" type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Bottles</label>
+            <select type="number" name="bottles" id="" value={bottles} onChange={(e) => setBottles(e.target.value)}>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <div>
+            <label>Time</label>
+            <select type="number" name="time" id="" value={time} onChange={(e) => setTime(e.target.value)}>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <div>
+            <label>Gender</label>
+            <input type="radio" name="gender" value="male" onChange={(e) => setGender(e.target.value)} />
+            <label>Male</label>
+            <input type="radio" name="gender" value="female" onChange={(e) => setGender(e.target.value)} />
+            <label>Female</label>
+          </div>
+          <div>
+            <output>{result.toFixed(1)}</output>
+          </div>
+          <button>Calculate</button>
+        </form>
+      </div>
     </div>
   );
 }
